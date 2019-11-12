02a3e0435f18ee1cc781b1f7e48fc1ac0b6da571
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2019-11-12 13:55:30 +0100

    add blue bar in index

--- resources/views/adminevents/index.blade.php
+++ resources/views/adminevents/index.blade.php
@@ -11,11 +11,15 @@
       <img class="welcome" src="/img/fond_events.jpg" alt="Events">
       <h1> PARTIPEZ À NOS ÉVÈNEMENTS ! </h1>
       
+      
 
     </aside>
+    <div class="blue_bar">
+    </div>
 
 
     <main>
+
       <article>
       <h2> Évènements actuels </h2>
       <h3> Inscrivez-vous à nos évènements du moment !</h2>
@@ -23,23 +27,26 @@
       
       @foreach ($events as $event)
 
-        <div class="image_event">  
-        <img class="event" src="/img/event.jpg" > 
-        </div>
-
         <div class="event">
-        
+        <img class="img_event" src="/img/event.jpg" > 
+        <div class=texte>
         <h3 class="event_date">{{ $event->date_event }} {{ $event->recurrence }}</h3>
         <p class="location">{{ $event->location }}</p>
         <h1 class="name">{{ $event->name }}</h1>
         <p class="description">{{ $event->description }}</p>
         
         <p>State: {{ $event->state }}</p>
-        <a class="show" href="/adminevents/{{ $event->id }}/edit"> En savoir plus </a>
+        <a class="show" href="/adminevents/{{ $event->id }}"> En savoir plus </a>
+        </div>
         </div>
     
       @endforeach
       </article>
+      <div class="blue_bar">
+    </div>
+    <article>
+      <h2> Évènements passés </h2>
+      <h3> Commentez et ajoutez des photos !</h2>
     </main>
   </body>
 </html>
