5511856c05a86270a5e06c8e9fbd991a9f14544a
lucas <ID+username@users.noreply.github.com>
2019-11-12 19:39:15 +0100

    Sample home page and image adding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,26 @@
 <head>
     <meta charset="utf-8">
     <title>BDE</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
 </head>
 <body>
-    TO DO
+    <!-- Include navbar -->
+
+    <article>
+        <div>
+            <img src="/img/58ab32e5a85a2.png" />
+            <h1>BDE CESI NICE</h1>
+        </div>
+
+        <div>
+            <h2><a href="./publicevents">Accédez à nos évènnements</a></h2>
+        </div>
+
+        <div>
+            <h2><a href="./shop">Accédez à notre boutique</a></h2>
+        </div>
+    </article>
+
+    <!-- Include footer -->
 </body>
 </html>
