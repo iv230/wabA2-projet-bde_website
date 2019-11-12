c1dce867f44a60e5e23859a2f518b4246a12d91a
lucas <ID+username@users.noreply.github.com>
2019-11-12 14:51:47 +0100

    Attemp to call API

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('articles', 'ArticleController');
+
+Route::resource('/users', 'UserController');
