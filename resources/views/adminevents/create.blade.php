84b1711aa74d528d9cb8dfd22646593adb5b3488
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2019-11-14 17:54:18 +0100

    Add event request to create Add administration button Add button create event Update migration event

--- resources/views/adminevents/create.blade.php
+++ resources/views/adminevents/create.blade.php
@@ -12,26 +12,32 @@
   @csrf
 
     <div>
+        {!! $errors->first('name', '<small>:message</small>') !!}
       <input type="text" name="name" placeholder="Event name">
     </div>
 
     <div>
+        {!! $errors->first('description', '<small>:message</small>') !!}
       <textarea name="description" placeholder=" Event description"></textarea>
     </div>
 
     <div>
+        {!! $errors->first('location', '<small>:message</small>') !!}
       <input type="text" name="location" placeholder="Event location">
     </div>
 
     <div>
+        {!! $errors->first('recurrence', '<small>:message</small>') !!}
       <input type="text" name="recurrence" placeholder="Event recurrence">
     </div>
 
     <div>
+        {!! $errors->first('date_event', '<small>:message</small>') !!}
       <input type="date" name="date_event" placeholder="Event date">
     </div>
 
     <div>
+        {!! $errors->first('price', '<small>:message</small>') !!}
       <input type="number" name="price" placeholder="Price">
     </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="utf-8">
     <title>BDE</title>
-    
+
     <link rel="stylesheet" href="./css/normalize.css">
     <link rel="stylesheet" href="./css/header.css">
     @yield('home_scss')
     @yield('index_scss')
-   
+
 </head>
 <body>
 <!-- NAV BAR -->
 <header role="header">
-        
+
         <nav class="menu" role="navigation">
             <div class="inner">
                     <div class="cesi">
@@ -22,7 +22,7 @@
                     <div class="m-left">
                             <h1 class="bde">bde cesi nice</h1>
                     </div>
-        
+
                     <div class="m-right">
                         <span class="searchbar">
                                 <input type="text" placeholder="Search..">
@@ -39,11 +39,12 @@
 
         </nav>
     <div class="nav_left">
-        
+
             <img class="logo_cesi_ingenieur" src="./img/logo_cesi.png" alt="logo cesi ingenieur">
-        
+            @yield('admin')
+
     </div>
-    
+
     </header>
 
     <!-- END OF NAV BAR -->
@@ -55,6 +56,7 @@
 
     @yield('link_event')
     @yield('actual_events')
+    @yield('create_event')
 
     <div class="blue_bar"></div>
 
