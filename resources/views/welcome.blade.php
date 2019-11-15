1b0614706a0e2640343882b8196254fdccde9323
lucas <ID+username@users.noreply.github.com>
2019-11-15 15:34:57 +0100

    Fixed base template resources

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <title>BDE</title>
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./css/normalize.css">
-    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/header.css">
     @yield('home_scss')
     @yield('index_scss')
 
@@ -18,7 +18,7 @@
         <nav class="menu">
             <div class="inner">
                     <div class="cesi">
-                            <img class="logocesi" src="./img/logo.png" alt="logo cesi">
+                            <img class="logocesi" src="/img/logo.png" alt="logo cesi">
                     </div>
                     <div class="m-left">
                             <h1 class="bde">bde cesi nice</h1>
@@ -28,7 +28,7 @@
                     <div class="md-form mt-0">
                              <input class="form-control" type="text" placeholder="Search" aria-label="Search">
                         </div>
-                        
+
                         <a href="#" class="m-link"><i class="fa fa-shopping-basket"></i> Panier</a>
                         <a href="users/create" class="m-link"><i class="fa fa-user-plus"></i> Inscription</a>
                         <a href="/login" class="m-link"><i class="fa fa-sign-in"></i> Connexion</a>
@@ -40,8 +40,8 @@
             </div>
 
         </nav>
-    <div class="nav_left">       
-            <img class="logo_cesi_ingenieur" src="./img/logo_cesi.png" alt="logo cesi ingenieur">
+    <div class="nav_left">
+            <img class="logo_cesi_ingenieur" src="/img/logo_cesi.png" alt="logo cesi ingenieur">
             @yield('admin')
 
     </div>
@@ -66,13 +66,13 @@
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="public/js/nav_bar.js" charset="utf-8"></script>
+    <script src="/js/nav_bar.js" charset="utf-8"></script>
 
 </body>
 <footer>
     <div class="logo_footer">
-        <img class="footer_cesi" src="./img/logo_cesi.png" alt="logo cesi ingenieur">
-        <img class="footer_bde" src="./img/logo.png" alt="logo cesi">
+        <img class="footer_cesi" src="/img/logo_cesi.png" alt="logo cesi ingenieur">
+        <img class="footer_bde" src="/img/logo.png" alt="logo cesi">
 
     </div>
     <a href="#" class="footer_link"> Mentions légales </a>
