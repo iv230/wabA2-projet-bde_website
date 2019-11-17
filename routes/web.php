9560b71cec33d9587c9b4be1c931b140b92d2c67
lucas <ID+username@users.noreply.github.com>
2019-11-17 13:22:09 +0100

    Started auth protection on AdminEvent

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('articles', 'ArticleController');
 Route::resource('categories', 'CategoryController');
 Route::get('/articles/{id}/delete', 'ArticleController@destroy');
 
-Route::resource('adminevents', 'EventController');
+Route::resource('adminevents', 'EventController')->middleware('App\Http\Middleware\AdminEventAuth');
 Route::resource('publicevents', 'PublicEventController');
 Route::resource('comments', 'CommentController');
 Route::get('participants', 'ParticipantController@index');
