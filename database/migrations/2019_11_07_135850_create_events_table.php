d9ebe114f2fee03214c4a330f97c7e826c5e7184
lucas <ID+username@users.noreply.github.com>
2019-11-18 12:16:03 +0100

    Fixed event description lenght in DB

--- database/migrations/2019_11_07_135850_create_events_table.php
+++ database/migrations/2019_11_07_135850_create_events_table.php
@@ -16,7 +16,7 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('location');
             $table->string('recurrence')->nullable();
             $table->boolean('state');
