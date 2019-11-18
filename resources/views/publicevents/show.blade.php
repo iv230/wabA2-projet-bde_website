385dcaad965f42975f47b4f82cae1c098f139f35
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2019-11-18 12:31:20 +0100

    Add button dl

--- resources/views/publicevents/show.blade.php
+++ resources/views/publicevents/show.blade.php
@@ -1,100 +1,104 @@
 @extends('template_welcome')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/show.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/show.css') }}" />
 @endsection
 
 @section('event')
-    <article class="events" >
+<article class="events">
 
-        <h1 class="name">{{ $event->name }}</h1>
-        <h3 class="event_date">{{ $event->date_event }} {{ $event->recurrence }}</h3>
-        <p class="location">{{ $event->location }}</p>
+    <h1 class="name">{{ $event->name }}</h1>
+    <h3 class="event_date">{{ $event->date_event }} {{ $event->recurrence }}</h3>
+    <p class="location">{{ $event->location }}</p>
 
-        <div class="event">
-            @if(isset($event->image))
-                <img class="img_event" src="{{ $event->image->path }}" alt="Image de couverture">
-            @else
-                <img class="img_event" src="/img/event.jpg" alt="Image de couverture">
-            @endif
-            <div class=texte>
-                <p class="description">{{ $event->description}}</p>
-            </div>
-        </div>
-        @if($event->price == 0)
-            <p class="price">Participation gratuite</p>
+    <div class="event">
+        @if(isset($event->image))
+        <img class="img_event" src="{{ $event->image->path }}" alt="Image de couverture">
         @else
-            <p class="price">Coût de l'évènement : {{$event->price}} EUR</p>
+        <img class="img_event" src="/img/event.jpg" alt="Image de couverture">
         @endif
+        <div class=texte>
+            <p class="description">{{ $event->description}}</p>
+        </div>
+    </div>
+    @if($event->price == 0)
+    <p class="price">Participation gratuite</p>
+    @else
+    <p class="price">Coût de l'évènement : {{$event->price}} EUR</p>
+    @endif
 
     @if(session()->has('user'))
-    @if (session('role') == 2)
-        <div class="participate">
-        <a href="/participants/{{ $event->id }}">Dl</a>
-    <!--<a href="http://localhost:3000/participants/{{ $event->id }}" download="Liste_des_participants.csv">Liste des participants</a>-->
-        </div>
+    @if (session('role') == 2 || session('role') == 4)
+    <div class="participate">
+        <a href="/participants/{{ $event->id }}">Télécharger la liste des participants</a>
+    </div>
     @endif
     @endif
     <div class="buttons_action">
-    <a class="like" href="">J'aime (15)</a><br>
-    @if ($event-> state == 1)
+        <a class="like" href="">J'aime (15)</a><br>
+        @if ($event-> state == 1)
 
-    <div class="participate">
-        <form action="/participants" method="POST">
-            @csrf
+        <div class="participate">
+            <form action="/participants" method="POST">
+                @csrf
 
-            <div>
-                <button type="submit"> Participer </button>
-            </div>
+                <div>
+                    <button type="submit"> Participer </button>
+                </div>
 
-            <input type="hidden" name="iduser" value="{{ session('user') }}">
+                <input type="hidden" name="iduser" value="{{ session('user') }}">
 
-            <input type="hidden" name="idevent" value="{{$event->id}}">
-        </form>
-    </div>
-    @endif
+                <input type="hidden" name="idevent" value="{{$event->id}}">
+            </form>
+        </div>
+        @endif
     </div> <br>
 
-    </article>
-    <a class="show" href="/publicevents"> Retourner à la liste des évènements </a>
+</article>
+<a class="show" href="/publicevents"> Retourner à la liste des évènements </a>
 @endsection
 
 
 @section('comments')
 
 
-    @if($event->state == 0)
-        <article class="comments">
+@if($event->state == 0)
+<article class="comments">
     @foreach ($comments as $comment)
     @if ($comment->id_event == $event->id)
-      <p class="comment">{{ $comment->comment_date }} | {{ $comment->autor }}: <br> {{ $comment->comment_content }}</p>
+    <p class="comment">{{ $comment->comment_date }} | {{ $comment->autor }}: <br> {{ $comment->comment_content }}</p>
+
+    @if(session()->has('user'))
+    @if(session('role') >= 3)
+    <button type="submit"> Supprimer </button>
+    @endif
+    @endif
     @endif
     @endforeach
 
     @if(session()->has('user'))
 
     <div class="to_comment">
-    <h3 class="t3">Commenter:</h3>
+        <h3 class="t3">Commenter:</h3>
 
-    <form action="/comments" method="POST">
-    {{ csrf_field() }}
+        <form action="/comments" method="POST">
+            {{ csrf_field() }}
 
-    <input type="hidden" name="autor" value="{{ session('username') }}">
-
-      <div>
-        {!! $errors->first('comment_content', '<small>:message</small>') !!}
-          <label for="comment_content">
-              <textarea name="comment_content" rows="3" cols="30" placeholder="..."></textarea>
-          </label>
-      </div>
+            <div>
+                {!! $errors->first('comment_content', '<small>:message</small>') !!}
+                <label for="comment_content">
+                    <textarea name="comment_content" rows="3" cols="30" placeholder="..."></textarea>
+                </label>
+            </div>
 
 
-        <button type="submit"> Ajouter </button>
-        <input type="hidden" name="idevent" value="{{$event->id}}">
+            <button type="submit"> Ajouter </button>
+            <input type="hidden" name="idevent" value="{{$event->id}}">
+            <input type="hidden" name="autor" value="{{ session('username') }}">
 
-    </form>
+        </form>
     </div>
     @endif
     @endif
-    </article>
+</article>
 @endsection
