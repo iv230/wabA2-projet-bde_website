b8c828eaad023ff93e7cf6f37fd958b7a581585c
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2019-11-18 23:26:25 +0100

    begin shop

---
+++ resources/views/adminshop/TRASH_show.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Article {{ $article->id }}</title>
+  </head>
+  <body>
+    <h1>Article {{ $article->id }}</h1>
+    <ul>
+      <li>Name: {{ $article->name }}</li>
+      <li>Price: {{ $article->price }}</li>
+      <li>Description: {{ $article->description }}</li>
+      <li>Catégorie: {{ $category->name }}</li>
+    </ul>
+    <form action="/shop/{{ $article->id }}/addtocart" method="POST">
+      <button type=submit>Ajouter au panier</button>
+    </form>
+    <div>
+      <input type='button' value='Supprimer' onclick="location.href='/adminshop/{{ $article->id }}/delete';">
+    </div>
+    <div>
+      <input type='button' value='Modifier' onclick="location.href='/adminshop/{{ $article->id }}/edit';">
+    </div>
+  </body>
+</html>
