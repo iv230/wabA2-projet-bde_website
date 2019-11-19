91eabfa6fbae5b93dc7f2d6a1fd68a448f985b7f
maxlink <maxime.bores@viacesi.fr>
2019-11-19 09:30:34 +0100

    Basket not finished not finished but it's too late

--- app/Article.php
+++ app/Article.php
@@ -9,6 +9,8 @@ class Article extends Model
     const UPDATED_AT = NULL;
     const CREATED_AT = NULL;
 
+    protected $fillable = ['name', 'description', 'price', 'category'];
+
     public function basket()
     {
         return $this->belongsToMany('App\Basket');
--- app/Basket.php
+++ app/Basket.php
@@ -9,6 +9,8 @@ class Basket extends Model
     const UPDATED_AT = NULL;
     const CREATED_AT = NULL;
 
+    protected $fillable = ['userId'];
+
     public function article()
     {
         return $this->belongsToMany('App\Article');
