a60924ebf69b3bac7621cfc32fa4a21de865cc31
lucas <ID+username@users.noreply.github.com>
2019-12-05 16:00:06 +0100

    Fixed user show displaying

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,25 +1,31 @@
 @extends('template_welcome')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index_users.css') }}" />
+@endsection
+
 @section('content')
 
-<h3>{{ $user->name }}</h3>
-<p>{{ $user->email }}</p>
-<ul>
-    <li>Centre: {{ $user->school }}</li>
-    <li>Role: {{ $user->role }}</li>
-</ul>
+<article class="users">
+    <h3>{{ $user->name }}</h3>
+    <p>{{ $user->email }}</p>
+    <ul>
+        <li>Centre: {{ $user->school }}</li>
+        <li>Role: {{ $user->role }}</li>
+    </ul>
 
-<a href="/users" type="button">Retour à l'index</a>
+    <a href="/users" type="button">Retour à l'index</a>
 
-@if(session()->has('user'))
-@if(session('user') == $user->id)
+    @if(session()->has('user'))
+    @if(session('user') == $user->id)
 
-<a href="/users/{{ $user->id }}/edit" type="button">Modifier</a><br />
-<form method="POST" action="/users/{{ $user->id }}">
-    {{ csrf_field() }}
-    {{ method_field('DELETE') }}
-    <input type="submit" value="Supprimer l'utilisateur" />
-</form>
+    <a href="/users/{{ $user->id }}/edit" type="button">Modifier</a><br />
+    <form method="POST" action="/users/{{ $user->id }}">
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+        <input type="submit" value="Supprimer l'utilisateur" />
+    </form>
+</article>
 
 @endif
 @endif
